6c8fe99c6bc93bbc8dc668fbbc57d0c2cc40a535
Syed25794 <syedgulmohammad25794@gmail.com>
2022-11-13 12:25:59 +0530

    backend files

---
+++ index.js
@@ -0,0 +1,35 @@
+
+const express = require('express');
+
+const app = express();
+
+const userController = require("./routes/user.routes");
+const productController= require("./routes/product.routes");
+const {connection} = require("./config/db")
+require("dotenv").config()
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 7000
+
+app.use("/product", productController)
+
+app.use("/user", userController);
+
+app.get("/",(req,res)=>{
+    res.redirect("/home");
+});
+
+
+
+app.listen(PORT, async () => {
+    try{
+        connection;
+        console.log("Connected to db")
+    }
+    catch(err){
+        console.log("Error connnecting to DB")
+        console.log(err)
+    }
+    console.log(`listening on PORT ${PORT}`)
+})
